1535079f4b0d6ccdd7159bf852c84d385c19dc20
freemjstudio <mjwoo0@naver.com>
2024-02-20 23:06:24 +0900

    [CodeTree] 트로미노

--- week4/우민지/[코드트리] 트로미노.py
+++ week4/우민지/[코드트리] 트로미노.py
@@ -4,12 +4,54 @@ answer = 0
 for i in range(n):
     grid.append(list(map(int, input().split())))
 
+types = [
+    [[1, 1, 0],
+     [1, 0, 0],
+     [0, 0, 0]],
+
+    [[1, 1, 0],
+     [0, 1, 0],
+     [0, 0, 0]],
+
+    [[1, 0, 0],
+     [1, 1, 0],
+     [0, 0, 0]],
+
+    [[0, 1, 0],
+     [1, 1, 0],
+     [0, 0, 0]],
+
+    [[1, 1, 1],
+     [0, 0, 0],
+     [0, 0, 0]],
+
+     [[1, 0, 0],
+     [1, 0, 0],
+     [1, 0, 0]]
+]
+
+# type 별로 탐색하기 & 최대합 구하기
+def solve(x, y):
+    max_total = 0 # max value
+    for i in range(6):
+        flag = True # 범위 확인
+        temp_total = 0
+        for dx in range(3):
+            for dy in range(3):
+                if types[i][dx][dy] == 0:
+                    continue
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m: # 범위 check
+                    temp_total += grid[nx][ny]
+                else:
+                    flag = False
+        if flag:
+            max_total = max(temp_total, max_total)
+    return max_total
+
+
 # 3*1 block
 for x in range(n):
     for y in range(m):
-        # 가로
-        if 0 <= (x + 2) < n and 0 <= (y + 2) < m:
-
-    # 세로
-
+        answer = max(answer, solve(x, y))
 print(answer)
