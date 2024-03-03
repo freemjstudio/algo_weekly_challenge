5771d026e454930c3cab0624b5437d1a77f0cc8e
freemjstudio <mjwoo0@naver.com>
2024-03-03 22:23:12 +0900

    CodeTree 컨베이어 밸트

--- week6/우민지/[코드트리] 컨베이어 벨트.py
+++ week6/우민지/[코드트리] 컨베이어 벨트.py
@@ -1,25 +1,22 @@
 n, t = map(int, input().split())
 arr = []
-
 for _ in range(2):
     arr.append(list(map(int, input().split())))
 
+
+for _ in range(t):
 # 오른쪽으로 shift
-temp = arr[0][n-1] # 3
-for i in range(n-1, 0, -1):
-    arr[0][i] = arr[0][i-1]
-# 아래로 shift
-temp2 = arr[1][n-1]
-arr[1][n-1] = temp
+    temp = arr[0][n-1] # 가장 오른쪽에 있는 숫자
+    for i in range(n-1, 0, -1):
+        arr[0][i] = arr[0][i-1]
 
-temp3 = arr[1][0]
-# 왼쪽으로 shift
-for i in range(n-2, 0, -1):
-    arr[1][i-1] = arr[1][i]
+    arr[0][0] = arr[1][n-1]
 
-# 위로 shift
-arr[0][0] = temp3
+    for i in range(n-1, 0, -1):
+        arr[1][i] = arr[1][i-1]
 
-# print
+    arr[1][0] = temp
+
+# print result
 for i in range(2):
     print(*arr[i])
