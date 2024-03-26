05dede0fadf7d5799da4a991c4209ee00820b5f8
freemjstudio <mjwoo0@naver.com>
2024-03-26 13:51:53 +0900

    CodeTree:가장 가까운 점

--- week10/우민지/[CodeTree] 가장 가까운 점.py
+++ week10/우민지/[CodeTree] 가장 가까운 점.py
@@ -1,5 +1,3 @@
-# https://www.codetree.ai/missions/8/problems/nearest-point/description
-
 import heapq
 
 n, m = map(int, input().split())
@@ -9,12 +7,15 @@ pq = []
 # 초기 입력
 for i in range(n):
     x, y = map(int, input().split())
-    heapq.heappush(pq, ((abs(x) + abs(x)),x, y))
+    heapq.heappush(pq, ((abs(x) + abs(y)),x, y))
 
 for i in range(m):
-    _ , min_x, min_y = heapq.heappop(pq)
-    min_x, min_y = min_x + 2, min_y + 2
-    heapq.heappush(pq, (abs(min_x) + abs(min_y) , min_x, min_y))
+    dist , min_x, min_y = heapq.heappop(pq) # 원점에서 가장 가까운 점
+    min_x += 2
+    min_y += 2
+    dist = abs(min_x) + abs(min_y)
+    heapq.heappush(pq, (dist , min_x, min_y))
 
-_, min_x, min_y = heapq.heappop(pq)
+
+dist, min_x, min_y = heapq.heappop(pq)
 print(min_x, min_y)
