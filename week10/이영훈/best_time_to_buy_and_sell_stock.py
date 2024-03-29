beb82c9612d9c4da154f8f1928b4815438a213bd
ANGHOOO <leeanghoo2@gmail.com>
2024-03-29 12:07:08 +0900

    [leetcode] best time to buy and sell stock

---
+++ week10/이영훈/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,11 @@
+from typing import List 
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                profit += prices[i+1] - prices[i]
+
+        return profit
+        
