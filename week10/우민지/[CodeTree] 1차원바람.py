6af42465a7c82bc4b486af1bc5712e230aa057d5
freemjstudio <mjwoo0@naver.com>
2024-03-30 09:54:52 +0900

    CodeTree 1차원 바람

--- week10/우민지/[CodeTree] 1차원바람.py
+++ week10/우민지/[CodeTree] 1차원바람.py
@@ -1,23 +1,22 @@
-## 2차 시도
+
 n, m, q = map(int, input().split()) # n * m
-arr = [[0] * (m+1) for _ in range(n+1)]
+arr = []
 
 SHIFT_RIGHT = 0
 SHIFT_LEFT = 1
 
-for row in range(1, n+1):
+for _ in range(n):
     line = list(map(int, input().split()))
-    for col, num in enumerate(line, start = 1):
-        arr[row][col] = num
+    arr.append(line)
 
 def shift(row, cur_dir):
     if cur_dir == SHIFT_RIGHT: # 오른쪽으로 밀어야 하는 경우
-        arr[row].insert(1, arr[row].pop())
+        arr[row].insert(0, arr[row].pop())
     else:
-        arr[row].insert(m, arr[row].pop(1))
+        arr[row].insert(m-1, arr[row].pop(0))
 
 def check_same_num(row1, row2):
-    for col in range(1, m+1):
+    for col in range(m):
         if arr[row1][col] == arr[row2][col]:
             return True
     return False
@@ -31,7 +30,8 @@ def simulate(start_row, start_dir):
 
     # 전파 시키기 - 위 방향
     cur_dir = start_dir
-    for row in range(start_row, 1, -1):
+
+    for row in range(start_row, 0, -1):
         if check_same_num(row, row-1):
             shift(row-1, cur_dir)
             cur_dir = flip_dir(cur_dir)
@@ -40,7 +40,7 @@ def simulate(start_row, start_dir):
 
     # 전파 시키기 - 아래 방향
     cur_dir = start_dir
-    for row in range(start_row, n):
+    for row in range(start_row, n-1):
         if check_same_num(row, row+1):
             shift(row+1, cur_dir)
             cur_dir = flip_dir(cur_dir)
@@ -48,14 +48,12 @@ def simulate(start_row, start_dir):
             break
 
 def print_result():
-    for row in range(1, n+1):
-        for col in range(1, m+1):
-            print(arr[row][col], end=" ")
-        print()
+    for i in range(n):
+        print(*arr[i])
 
 for _ in range(q):
     row, wind_dir = input().split()
-    row = int(row)
+    row = int(row) - 1
     if wind_dir == 'L':
         cur_dir = SHIFT_RIGHT
     else:
