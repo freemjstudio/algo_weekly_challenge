d467e96a08f4a975ba2daec120a3f2ad203bdc7e
freemjstudio <mjwoo0@naver.com>
2024-04-07 12:24:48 +0900

    LeetCode Delete the Middle Node

--- week11/우민지/[CodeTree] 왕실의 기사 대결.py
+++ week11/우민지/[CodeTree] 왕실의 기사 대결.py
@@ -1,4 +1,6 @@
 # https://www.codetree.ai/training-field/frequent-problems/problems/royal-knight-duel/description?page=1&pageSize=20
+from collections import deque
+
 L, N, Q = map(int, input().split())
 # 상하좌우
 dx = [-1, 1, 0, 0]
@@ -28,9 +30,14 @@ for idx in range(1, N + 1):
     nights[idx] = Night(idx, r, c, h, w, k)
 
 
-def check_avalible(now):
+def check_avalible(now, d):
     flag = True
     # bfs 진행하기
+    queue = deque()
+    queue.append(now)
+    while queue:
+        now = queue.popleft()
+        for i in range():
 
     return flag
 
@@ -42,13 +49,15 @@ def update_info():
 def simulate(idx, d):
     global answer
     # 해당 기사 찾기
+    now = nights[idx]
+    if now.k > 0:  # 아직 살아 있는지 확인
 
-    # 밀칠 수 있는지 여부 확인
 
-    # 밀치기 , 좌표와 체력 업데이트 연쇄적으로 하기
+# 밀칠 수 있는지 여부 확인
 
-    # 피해량을 구하기
+# 밀치기 , 좌표와 체력 업데이트 연쇄적으로 하기
 
+# 피해량을 구하기
 
 for _ in range(Q):
     idx, d = map(int, input().split())
