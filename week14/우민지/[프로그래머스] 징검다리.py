e1bc78aad3256ebd047b524505aa79387db1a0c3
freemjstudio <mjwoo0@naver.com>
2024-04-25 21:08:18 +0900

    프로그래머스 징검다리

--- week14/우민지/[프로그래머스] 징검다리.py
+++ week14/우민지/[프로그래머스] 징검다리.py
@@ -1,25 +1,27 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/43236
-
-from itertools import combinations
-
-
 def solution(distance, rocks, n):
     answer = 0
+
+    rocks.append(distance)
     rocks.sort()
+    left, right = 0, distance
 
-    comb = combinations(rocks, len(rocks) - n)
+    while left <= right:
+        mid = (left + right) // 2
+        remove_rock_cnt = 0  # 제거한 바위 수
 
-    # 모든 경우 확인
-    for com in comb:
-        min_dist = distance
-        for i in range(len(com)):
-            if i == 0:
-                min_dist = min(min_dist, com[i])
-            elif i == len(com) - 1:
-                min_dist = min(min_dist, distance - com[i])
+        prev_rock = 0
+        for rock in rocks:
+            if (rock - prev_rock) < mid:  # 기준보다 작으면 제거하기 (기준은 거리의 최솟값임)
+                remove_rock_cnt += 1
+                if remove_rock_cnt > n:  # 이미 초과해서 제거한 경우 fail 이므로 멈추기
+                    break
             else:
-                min_dist = min(min_dist, com[i] - com[i - 1])
+                prev_rock = rock  # 갱신하기
 
-        answer = max(min_dist, answer)
+        if remove_rock_cnt > n:  # 너무 많이 삭제 -> mid 커트라인이 너무 높다
+            right = mid - 1
+        else:
+            answer = max(answer, mid)
+            left = mid + 1
 
     return answer
